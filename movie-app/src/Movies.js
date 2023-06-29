3f74ef484c3f27254f4445905742d7b6bcb5e945
Flash1105-2 <flashpay110599@gmail.com>
2023-06-28 20:45:34 -0400

    adds titles

---
+++ movie-app/src/Movies.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Movies = () => {
+    const {movies, setMovies} = useState([
+    { title: 'Star Wars Episode I: The Phantom Menace', director: 'George Lucas' },
+    { title: 'Star Wars Episode II: Attack of the clones', director: 'George Lucas'},
+    { title: 'Star Wars Episode III: Revenge of the Sith', director: 'George Lucas'}
+
+
+]);
+return (
+<div>
+    <h1>Best movies</h1>
+    <ul>
+    {movies.map((movie, index) => (
+        <li key={index}>
+            <strong>Title:</strong> {movie.title} | <strong>Director:</strong> {movie.director}
+        </li>
+    ))}
+    </ul>
+</div>
+);
+    };
+
+    export default Movies;
