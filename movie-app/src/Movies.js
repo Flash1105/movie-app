79d95d24c56d87735968c27edad36ec1ae0b0289
Flash1105-2 <flashpay110599@gmail.com>
2023-06-28 20:59:38 -0400

    creates app

--- movie-app/src/Movies.js
+++ movie-app/src/Movies.js
@@ -4,8 +4,13 @@ const Movies = () => {
     const {movies, setMovies} = useState([
     { title: 'Star Wars Episode I: The Phantom Menace', director: 'George Lucas' },
     { title: 'Star Wars Episode II: Attack of the clones', director: 'George Lucas'},
-    { title: 'Star Wars Episode III: Revenge of the Sith', director: 'George Lucas'}
-
+    { title: 'Star Wars Episode III: Revenge of the Sith', director: 'George Lucas'},
+    { title: 'Star Wars Episode IV: A New Hope', director: 'George Lucas'},
+    { title: '', director: ''},
+    { title: '', director: ''},
+    { title: '', director: ''},
+    { title: '', director: ''},
+    { title: '', director: ''},
 
 ]);
 return (
